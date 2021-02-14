d24b64ca938040a89ad376295f89410f5122fb07
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2021-02-14 13:10:12 -0800

    service worker made

---
+++ Develop/public/js/idb.js
@@ -0,0 +1,67 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1);
+request.onupgradeneeded = function (event) {
+    const db = event.target.results;
+    db.createObjectStore('new_transaction', { autoIncrement: true }); 0
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        uploadTransaction();
+    }
+};
+
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    transactionObjectStore.add(record);
+}
+
+function uploadTransaction() {
+    //open a transaction on your db
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    //access you object store
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    //get all records from store and set to a variable
+    const getAll = transactionObjectStore.getAll();
+
+    //upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function () {
+        //if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+            fetch('/routes/api', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // open one more transaction
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+                    //access the new_pizza object store
+                    const transactionObjectStore = transaction.objectStore('new_transaction');
+                    //clear all items in your store
+                    transactionObjectStore.clear();
+
+                    alert('All saved transactions have been submitted!')
+                })
+                .catch(err => {
+                    consol.log(err)
+                });
+        }
+    };
+}
